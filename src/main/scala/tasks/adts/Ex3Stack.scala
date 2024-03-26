a6e83e5140207c22cc69e15ff298be2fde53757f
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-03-26 18:38:03 +0100

    Complete ex3

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -8,7 +8,7 @@ import u03.Optionals.*
  *  Suggestion: 
  *  - push adds an element and returns the new stack
  *  - pop returns:
- *  -- empty optional is stack is empty
+ *  -- empty optional if stack is empty
  *  -- a pair of top of the stack and the new stack after removal if not empty
  */
 object Ex3Stacks:
@@ -22,9 +22,18 @@ object Ex3Stacks:
       def asSequence(): Sequence[A]
   
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    import Sequence.*
+    import Optional.*
+
+    type Stack[A] = Sequence[A]
+    def empty[A]: Stack[A] = Nil()
+
     extension [A](stack: Stack[A])
-      def push(a: A): Stack[A] = ???
-      def pop(a: A): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+      def push(a: A): Stack[A] = stack match
+        case _ => Cons(a, stack)
+      
+      def pop(a: A): Optional[(A, Stack[A])] = stack match
+        case Cons(h, t) => Just(h, t)
+        case _ => Empty()
+      
+      def asSequence(): Sequence[A] = stack
