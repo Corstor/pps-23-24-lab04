afcafab9cfa7646fad0469123e22ff5792369d8a
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-03-27 16:15:18 +0100

    Complete ex5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package u04lab
 import u03.Sequences.* 
 import Sequence.*
+import u03.Optionals.* 
+import Optional.*
+import u04lab.Ex5Traversable.logAll
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -18,9 +21,27 @@ import Sequence.*
 object Ex5Traversable:
 
   def log[A](a: A): Unit = println("The next element is: "+a)
-
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
-
   
+  trait Traversable[T[_]]:
+    def consume[A](t: T[A])(f: A => Unit): Unit
+
+  given Traversable[Optional] with
+    def consume[A](t: Optional[A])(f: A => Unit): Unit = t match
+      case Just(v) => f(v)
+      case _ => ()
+
+  given Traversable[Sequence] with
+    def consume[A](t: Sequence[A])(f: A => Unit): Unit = t match
+      case Cons(h, t) => f(h); consume(t)(f)
+      case _ => ()
+
+  def logAll[T, A[_]: Traversable](t: A[T]): Unit =
+    val traversable = summon[Traversable[A]]
+    traversable.consume(t)(log)
+    
+@main def test =
+  val seq: Sequence[Int] = Cons(5, Cons(7, Cons(10 , Nil())))
+  val opt: Optional[Int] = Just(100)
+  
+  logAll(seq)
+  logAll(opt)
